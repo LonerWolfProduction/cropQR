0b3d5736cb0df955286c515e142bda8a3c292dc8
LWProd <48309226+LonerWolfProduction@users.noreply.github.com>
2025-01-26 22:30:18 +0500

    hot-fix

---
+++ cropQR_v0-5.py
@@ -0,0 +1,46 @@
+#python3
+from PIL import Image
+from pyzbar.pyzbar import decode
+import string
+import sys
+import glob
+
+visible_out = 1
+if __name__ == "__main__":
+    if len (sys.argv) == 1:
+        print ("if you need help, you need to use the --help (-h) command")
+    else:
+        param_name = sys.argv[1]
+
+        if (param_name == "--help" or
+                param_name == "-h"):
+            print ("Launch parameters and arguments:\n--help (-h) : Arguments and launch parameters\n--here (-H) : Perform decoding inside the current folder\n--silent (-s) : decode decoding is not accompanied by output to the terminal")
+            sys.exit (1)
+
+        if len (sys.argv) < 2:
+            print ("if you need help, you need to use the --help (-h) command")
+            sys.exit (1)
+
+        if len (sys.argv) > 3:
+            print ("if you need help, you need to use the --help (-h) command")
+            sys.exit (1)
+
+        if len (sys.argv) == 3:
+            if str(sys.argv[2]) == "--silent" or str(sys.argv[2]) == "-s":
+                visible_out = 0
+
+        if (param_name == "--here" or
+                param_name == "-H" or
+                param_value == "--here" or
+                param_value == "H"):
+            fout = open("cropQRdata.txt", "a")
+            for file in glob.glob(r'*.png'):
+                name = file
+                result = str(decode(Image.open(name)))
+                outdata = str(result)
+                if visible_out == 1: print (outdata)
+                fout.write(outdata + "\n")
+            fout.close()
+        else:
+            print ("if you need help, you need to use the --help (-h) command")
+            sys.exit (1)
